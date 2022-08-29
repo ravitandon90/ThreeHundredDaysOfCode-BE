23055c8cdf779ee19587b9c024aa503a9eb457c8
Ravi Tandon <ravi@decoverhq.com>
2022-08-29 16:46:18 +0000

    Fix in name of a problem

--- master/src/main/java/com/code/master/common/AddLikeHTTPRequest.java
+++ master/src/main/java/com/code/master/common/AddLikeHTTPRequest.java
@@ -3,8 +3,7 @@ package com.code.master.common;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Getter @Setter
 public class AddLikeHTTPRequest {
     private String postId;
     private String userId;
