e8d9d0ec8608fd414bbb91fbc44c3fc65df1c4b3
Ravi Tandon <ravi@decoverhq.com>
2022-08-29 08:24:49 +0000

    Basic definition of AddLike and AddComment HTTP requests.

--- master/src/main/java/com/code/master/common/AddCommentHTTPRequest.java
+++ master/src/main/java/com/code/master/common/AddCommentHTTPRequest.java
@@ -1,4 +1,11 @@
 package com.code.master.common;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class AddCommentHTTPRequest {
+    private String userId;
+    private String text;
+    private String postId;
 }
--- master/src/main/java/com/code/master/common/AddLikeHTTPRequest.java
+++ master/src/main/java/com/code/master/common/AddLikeHTTPRequest.java
@@ -1,4 +1,12 @@
 package com.code.master.common;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AddLikeHTTPRequest {
+    private String postId;
+    private String userId;
+    private String isLiked;
 }
