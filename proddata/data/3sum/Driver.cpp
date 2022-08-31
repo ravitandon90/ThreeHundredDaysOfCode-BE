e73a365b8b63b8105a7d6e7dd1b77f224b9d0ad9
Ravi Tandon <ravi@decoverhq.com>
2022-08-31 15:16:57 -0400

    Added driver for C++ (3-Sum).

---
+++ proddata/data/3sum/Driver.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <fstream>
+#include<iostream>
+#include <stack>
+#include<string>
+#include<vector>
+#include <boost/tokenizer.hpp>
+
+#include "Solution.hpp"
+#include "../../cpp/helper.hpp"
+
+using namespace std;
+using namespace boost;
+
+int main() {
+    int numInputs;
+    vector<int> input;
+    vector<vector<int> > expected_output;
+    vector<vector<int> > actual_output;
+    ifstream infile("testcases.txt");
+    int lineNo = -1;
+    Solution* s = new Solution();
+    string line;
+    while(getline(infile, line)) {
+      lineNo++;
+      if (lineNo == 0) {
+        numInputs = stoi(line);
+      } else if ((lineNo % 2) == 1) {
+        input = ParseLineToVector(line);
+      } else {
+        expected_output = ParseLineToVectorOfVectors(line);
+        actual_output = s->threeSum(input);
+        if (not Compare(expected_output, actual_output)) {
+          cout << "Actual Output:" << Print(actual_output) << endl;
+          cout << "Expected Output:" << Print(expected_output) << endl;
+        }
+      }
+    }
+    return 0;
+}
