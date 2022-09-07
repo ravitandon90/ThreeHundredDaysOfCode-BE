75f6f9a45445d4eee6c2525fc4c156ec8680920b
Lokesh Saana <lokeshsurya544@gmail.com>
2022-09-07 10:56:06 +0530

    proddata/data/basic-calculator-ii/Python/ files have been added

---
+++ proddata/data/basic-calculator-ii/Python/Driver.py
@@ -0,0 +1,23 @@
+from Solution import Solution
+import sys
+import os
+
+file1 = open(os.path.join(sys.path[0], 'testcases.txt'), 'r')
+Lines = file1.readlines()
+lineNo = 1
+status = 0
+obj = Solution()
+for line in Lines:
+    if (lineNo == 1):
+        actual_output = obj.calculate(line)
+        lineNo = 0
+    else:
+        expected_output = line.strip()
+        lineNo = 1
+        if (expected_output != str(actual_output)):
+            print('Actual Output:', actual_output)
+            print('Expected Output:', expected_output)
+            status = 1
+            break
+if status == 0:
+    print('Accepted')
