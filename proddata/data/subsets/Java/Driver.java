f1632bb7ba17cfc862661b03d07538002aecd2ff
Madhavv024 <madhavvarjuntthukral@gmail.com>
2022-09-11 02:23:27 +0530

    added driver for subset

---
+++ proddata/data/subsets/Java/Driver.java
@@ -0,0 +1,127 @@
+package subsets.Java;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Driver
+{
+    private static List<String> method(String filePath) {
+        StringBuilder builder = new StringBuilder();
+        ArrayList<String> al = new ArrayList<>();
+
+        try (BufferedReader buffer = new BufferedReader(
+                new FileReader(filePath))) {
+
+            String str;
+            while ((str = buffer.readLine()) != null) {
+                al.add(str);
+            }
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+//        System.out.println(al);
+        // Returning a string arraylist
+        return al;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(drivercode() ? "Accepted" : "Wrong Answer");
+        ;
+    }
+
+    static boolean drivercode() {
+        String filePath = "src/subsets/testcases.txt";
+        List<String> al = method(filePath);
+
+        List<List<Integer>> out = new ArrayList<>();
+        int input1[] = new int[0];
+
+        boolean b = true;
+
+        for (int i = 0; i< al.size();i++)
+        {
+            if(al.get(i).equals("input"))
+            {
+                String s[] = al.get(i+1).split(" ");
+                input1 = Arrays.stream(s).mapToInt(Integer::parseInt).toArray();
+//                System.out.println("input array is--"+Arrays.toString(input1));
+                i++;
+
+            }
+            else if(al.get(i).equals("output") || !al.get(i).equals("check"))
+            {
+                List<Integer> al2 = new ArrayList<>();
+                if(al.get(i+1).equals(""))
+                {
+//                    out.add(al2);
+
+                }
+                else
+                {
+                    String s= al.get(i);
+//                    System.out.println("String is--"+s);
+                    StringTokenizer st = new StringTokenizer(s);
+                    int n = st.countTokens();
+                    for(int j = 0;j<n;j++)
+                    {
+                        al2.add(Integer.parseInt(st.nextElement().toString()));
+                    }
+                    out.add(al2);
+
+                }
+            }
+            else if(al.get(i).equals("check"))
+            {
+//                System.out.println("input is--" + Arrays.toString(input1));
+//                System.out.println("output is---"+out);
+                List<List<Integer>> user_out = subsets(input1);
+
+                boolean chec = true;
+                chec = chec & out.size()==user_out.size();
+
+                out.sort(comparator);
+                out.stream().map(l -> {
+                    Collections.sort(l, Comparator.naturalOrder()); return l;
+                }).collect(Collectors.toList());
+
+                user_out.sort(comparator);
+                user_out.stream().map(l -> {
+                    Collections.sort(l, Comparator.naturalOrder()); return l;
+                }).collect(Collectors.toList());
+
+                for (int k=0;k<user_out.size();k++)
+                {
+                    chec = chec & out.get(k).equals(user_out.get(k));
+                }
+                b = b & chec & user_out.equals(out);
+                if (b == false) {
+                    System.out.println("Expected output  " + out);
+                    System.out.println("Your output  " + user_out);
+                    return b;
+                }
+
+                out.clear();
+            }
+        }
+        return b;
+    }
+
+    public static Comparator<List<Integer>> comparator = (x, y) -> {
+        for (int i = 0; i < Math.min(x.size(), y.size()); i++) {
+            if (x.get(i) != y.get(i)) {
+                return x.get(i) - y.get(i);
+            }
+        }
+        return Integer.compare(x.size(), y.size());
+    };
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        Solution sol = new Solution();
+        return sol.subsets(nums);
+    }
+}
