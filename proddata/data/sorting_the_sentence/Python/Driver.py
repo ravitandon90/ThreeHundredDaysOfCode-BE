745fe3af970b061e190759ce5289079ddaf0b1ad
Swapnil Sengupta <53232360+Swapnil-2001@users.noreply.github.com>
2022-09-18 03:54:08 +0530

    Add driver programs in Python from 22nd Jan, 2023 to 27th Jan, 2023 (#49)
    Co-authored-by: Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>

---
+++ proddata/data/sorting_the_sentence/Python/Driver.py
@@ -0,0 +1,26 @@
+from Solution import Solution
+import sys
+import os
+
+file = open(os.path.join(sys.path[0], '../testcases.txt'), 'r')
+lines = file.readlines()
+
+isSolutionWrong = False
+lineNumber = 1
+solution = Solution()
+for line in lines[1:]:
+    if lineNumber % 2 == 1:
+        input = line.strip()[1:-1].strip()
+        actualOutput = solution.sortSentence(input)
+    else:
+        expectedOutput = line.strip()[1:-1].strip()
+        if (actualOutput != expectedOutput):
+            print("Result: Failed")
+            print("Actual Output: ", actualOutput)
+            print("Expected Output: ", expectedOutput)
+            isSolutionWrong = True
+            break
+    lineNumber += 1
+
+if not isSolutionWrong:
+    print("Result: Success")
