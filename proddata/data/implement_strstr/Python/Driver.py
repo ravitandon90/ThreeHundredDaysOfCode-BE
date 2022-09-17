e5dc4d357a1227d02c4ee38e5cbeafa304ad1918
Swapnil Sengupta <53232360+Swapnil-2001@users.noreply.github.com>
2022-09-18 03:54:12 +0530

    Add driver code in Python for 6 problems between 29th Jan to 5th Feb (#48)
    Co-authored-by: Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>

---
+++ proddata/data/implement_strstr/Python/Driver.py
@@ -0,0 +1,26 @@
+from Solution import Solution
+import sys
+import os
+
+file = open(os.path.join(sys.path[0], '../testcases.txt'), 'r')
+lines = file.readlines()
+
+isSolutionWrong = False
+lineNumber = 1
+solution = Solution()
+for line in lines[1:]:
+    if lineNumber % 2 == 1:
+        a, b = [s.strip()[1:-1].strip() for s in line.strip().split(',')]
+        actualOutput = solution.strStr(a, b)
+    else:
+        expectedOutput = int(line.strip())
+        if (actualOutput != expectedOutput):
+            print("Result: Failed")
+            print("Actual Output: ", actualOutput)
+            print("Expected Output: ", expectedOutput)
+            isSolutionWrong = True
+            break
+    lineNumber += 1
+
+if not isSolutionWrong:
+    print("Result: Success")
