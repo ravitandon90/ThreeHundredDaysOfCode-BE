050f771468082262c57bcd7a4821e3c10a7a03a4
Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>
2022-09-18 13:02:04 +0530

    Add driver programs for problems from 31st October to 9th November

---
+++ proddata/data/can_place_flowers/Python/Driver.py
@@ -0,0 +1,32 @@
+from Solution import Solution
+import sys
+import os
+
+file = open(os.path.join(sys.path[0], '../testcases.txt'), 'r')
+lines = file.readlines()
+
+isSolutionWrong = False
+lineNumber = 1
+solution = Solution()
+for line in lines[1:]:
+    line = line.strip()
+    if lineNumber % 2 == 1:
+        pos = len(line) - 1
+        while line[pos] != ',':
+            pos -= 1
+        n = int(line[pos + 1:].lstrip())
+        flowerbed = list(
+            map(int, [s.strip() for s in line[:pos].rstrip()[1:-1].split(',')]))
+        actualOutput = solution.canPlaceFlowers(flowerbed, n)
+    else:
+        expectedOutput = True if line == "true" else False
+        if (actualOutput != expectedOutput):
+            print("Result: Failed")
+            print("Actual Output: ", actualOutput)
+            print("Expected Output: ", expectedOutput)
+            isSolutionWrong = True
+            break
+    lineNumber += 1
+
+if not isSolutionWrong:
+    print("Result: Success")
---
+++ proddata/data/continuous_subarray_sum/Python/Driver.py
@@ -0,0 +1,37 @@
+from Solution import Solution
+import sys
+import os
+
+file = open(os.path.join(sys.path[0], '../testcases.txt'), 'r')
+lines = file.readlines()
+
+isSolutionWrong = False
+lineNumber = 1
+solution = Solution()
+for line in lines[1:]:
+    line = line.strip()
+    if lineNumber % 2 == 1:
+        pos = len(line) - 1
+        while line[pos] != '=':
+            pos -= 1
+        k = int(line[pos + 1:].lstrip())
+        while line[pos] != ']':
+            pos -= 1
+        ind = 4
+        while line[ind] != '[':
+            ind += 1
+        nums = list(map(int, [s.strip()
+                    for s in line[ind + 1: pos].split(',')]))
+        actualOutput = solution.checkSubarraySum(nums, k)
+    else:
+        expectedOutput = True if line == "true" else False
+        if (actualOutput != expectedOutput):
+            print("Result: Failed")
+            print("Actual Output: ", actualOutput)
+            print("Expected Output: ", expectedOutput)
+            isSolutionWrong = True
+            break
+    lineNumber += 1
+
+if not isSolutionWrong:
+    print("Result: Success")
