1fe4548773e7a4e840f41794af512ac256bcfa9b
Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>
2022-09-18 18:43:56 +0530

    Add driver programs in Python for all problems from 30th Nov to 8th December

---
+++ proddata/data/kth_largest_element_in_an_array/Python/Driver.py
@@ -0,0 +1,33 @@
+from Solution import Solution
+import sys
+import os
+
+file = open(os.path.join(sys.path[0], '../testcases.txt'), 'r')
+lines = file.readlines()
+
+isSolutionWrong = False
+lineNumber = 1
+solution = Solution()
+for line in lines[1:]:
+    line = line.strip()
+    if lineNumber % 2 == 1:
+        testCase = line
+        pos = len(line) - 1
+        while line[pos] != ',':
+            pos -= 1
+        k = int(line[pos + 1:].lstrip())
+        nums = list(map(int, [s.strip()
+                    for s in line[:pos].rstrip()[1:-1].split(',')]))
+        actualOutput = solution.findKthLargest(nums, k)
+    else:
+        expectedOutput = int(line)
+        if (actualOutput != expectedOutput):
+            print("Result: Failed for test case: " + testCase)
+            print("Actual Output: ", actualOutput)
+            print("Expected Output: ", expectedOutput)
+            isSolutionWrong = True
+            break
+    lineNumber += 1
+
+if not isSolutionWrong:
+    print("Result: Success")
---
+++ proddata/data/top_k_frequent_words/Python/Driver.py
@@ -0,0 +1,34 @@
+from Solution import Solution
+import sys
+import os
+
+file = open(os.path.join(sys.path[0], '../testcases.txt'), 'r')
+lines = file.readlines()
+
+isSolutionWrong = False
+lineNumber = 1
+solution = Solution()
+for line in lines[1:]:
+    line = line.strip()
+    if lineNumber % 2 == 1:
+        testCase = line
+        pos = len(line) - 1
+        while line[pos] != ',':
+            pos -= 1
+        k = int(line[pos + 1:].lstrip())
+        words = [s.strip()[1:-1].strip()
+                 for s in line[:pos].rstrip()[1:-1].split(',')]
+        actualOutput = solution.topKFrequent(words, k)
+    else:
+        expectedOutput = [s.strip()[1:-1].strip()
+                          for s in line[1:-1].split(',')]
+        if (actualOutput != expectedOutput):
+            print("Result: Failed for test case: " + testCase)
+            print("Actual Output: ", actualOutput)
+            print("Expected Output: ", expectedOutput)
+            isSolutionWrong = True
+            break
+    lineNumber += 1
+
+if not isSolutionWrong:
+    print("Result: Success")
