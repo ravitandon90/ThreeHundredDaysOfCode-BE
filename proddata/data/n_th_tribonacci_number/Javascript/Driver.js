d2b56901c60883d7b46a8d8496b19ea0b60bcc5b
Hritik Sharma <sharmahritik2002@gmail.com>
2022-09-18 22:51:26 +0530

    Add 18 Driver.js program

---
+++ proddata/data/n_th_tribonacci_number/Javascript/Driver.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const { Solution } = require("./Solution");
+const {
+  compare1DArray,
+  convertStringArrToNumArr,
+  getArrFromStr,
+} = require("../../../javascript/helper");
+
+let testcases = [];
+//  testcases: array of objects
+//  {
+//      arg1: Number,
+//      output: Number
+// }
+let Success = true;
+getTestCases();
+
+const sizeOfTestCases = testcases.length;
+main();
+
+function main() {
+  for (let i = 0; i < sizeOfTestCases; i++) {
+    const user_code_output = Solution(testcases[i].arg1);
+
+    if (user_code_output !== testcases[i].output) {
+      console.log("Result: Failed");
+      console.log("Failed test case:", testcases[i].arg1);
+      console.log("Output:", user_code_output);
+      console.log("Expected:", testcases[i].output);
+      Success = false;
+      break;
+    }
+  }
+
+  if (Success) {
+    console.log("Result: Success");
+  }
+}
+
+function getTestCases() {
+  try {
+    let data = fs.readFileSync(
+      "proddata/data/n_th_tribonacci_number/testcases.txt",
+      "utf8"
+    );
+    data = data.split("\n");
+    data.shift();
+
+    for (let i = 0; i < data.length; i++) {
+      let input1 = Number(data[i].trim());
+      // jumps to output of that testcase
+      i++;
+
+      let output = Number(data[i].trim());
+
+      testcases.push({
+        arg1: input1,
+        output: output,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
--- proddata/javascript/helper.js
+++ proddata/javascript/helper.js
@@ -14,6 +14,38 @@ function compare1DArray(arr1 = [], arr2 = []) {
   return true;
 }
 
+// converts string elements of array to number and returns a new array
+function convertStringArrToNumArr(strArr) {
+
+  let arr = [];
+  arr = strArr.map((val) => {
+    return Number(val);
+  });
+
+  return arr;
+}
+
+// converts string into array
+function getArrFromStr(str) {
+  let i = 0,
+    arr = [];
+  while (i < str.length && str[i] != "[") {
+    i++;
+  }
+  i++;
+
+  while (i < str.length && str[i] != "]") {
+    if (str[i] == " " || str[i] == ",") {
+    } else arr.push(str[i]);
+
+    i++;
+  }
+
+  return arr;
+}
+
 module.exports = {
   compare1DArray,
+  convertStringArrToNumArr,
+  getArrFromStr,
 };
