b0ea216a2721fb6d3d918630a25b99ba62771edd
Swapnil Sengupta <53232360+Swapnil-2001@users.noreply.github.com>
2022-09-19 17:17:58 +0530

    Add driver programs in Python for all problems from 30th Nov to 8th December (#65)
    Co-authored-by: Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>

---
+++ proddata/data/non_overlapping_intervals/Python/Driver.py
@@ -0,0 +1,35 @@
+from Solution import Solution
+import sys
+import os
+
+file = open(os.path.join(sys.path[0], '../testcases.txt'), 'r')
+lines = file.readlines()
+
+isSolutionWrong = False
+lineNumber = 1
+solution = Solution()
+for line in lines[1:]:
+    line = line.strip()
+    if lineNumber % 2 == 1:
+        testCase = line
+        input = []
+        splitArrays = line[1:-1].strip()[:-1].split(']')
+        for array in splitArrays:
+            pos = 0
+            while array[pos] != '[':
+                pos += 1
+            input.append(
+                list(map(int, [s.strip() for s in array[pos + 1:].strip().split(',')])))
+        actualOutput = solution.eraseOverlapIntervals(input)
+    else:
+        expectedOutput = int(line)
+        if (actualOutput != expectedOutput):
+            print("Result: Failed for test case: " + testCase)
+            print("Actual Output: ", actualOutput)
+            print("Expected Output: ", expectedOutput)
+            isSolutionWrong = True
+            break
+    lineNumber += 1
+
+if not isSolutionWrong:
+    print("Result: Success")
