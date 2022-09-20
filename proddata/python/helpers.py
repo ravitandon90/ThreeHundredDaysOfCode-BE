0f17e930494dd26261183fbf9a5af1fa9fa6e0ef
Swapnil Sengupta <53232360+Swapnil-2001@users.noreply.github.com>
2022-09-20 17:17:05 +0530

    Add 10 more driver programs in Python for binary tree problems (#82)
    * Add 10 more driver programs in Python for binary tree problems
    
    * Create helper functions and share them between the driver programs for tree problems in Python
    
    * Add the file containing the helper functions
    
    * Clean up the code and remove unnecessary imports
    
    Co-authored-by: Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>

---
+++ proddata/python/helpers.py
@@ -0,0 +1,73 @@
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def createTreeFromArrayInput(arrayInput):
+    root = None
+    inputWithNoParentheses = arrayInput[1:-1].strip()
+    if inputWithNoParentheses == "":
+        return root
+    stringNodes = [stringFormOfNode.strip()
+                   for stringFormOfNode in inputWithNoParentheses.split(',')]
+    return createTree(stringNodes)
+
+
+def createTree(stringNodes):
+    root = TreeNode(int(stringNodes[0]))
+    posInArray, q = 1, deque([root])
+    while len(q) > 0:
+        size = len(q)
+        while size > 0:
+            currentNode = q.popleft()
+            if posInArray == len(stringNodes):
+                q = []
+                break
+            if stringNodes[posInArray] != "null":
+                currentNode.left = TreeNode(int(stringNodes[posInArray]))
+                q.append(currentNode.left)
+            posInArray += 1
+            if posInArray == len(stringNodes):
+                q = []
+                break
+            if stringNodes[posInArray] != "null":
+                currentNode.right = TreeNode(int(stringNodes[posInArray]))
+                q.append(currentNode.right)
+            posInArray += 1
+            size -= 1
+    return root
+
+
+def createArrayFromTreeInput(root):
+    if not root:
+        return []
+    array, q = [str(root.val)], deque([root])
+    while len(q) > 0:
+        size = len(q)
+        while size > 0:
+            currentNode = q.popleft()
+            if currentNode.left:
+                q.append(currentNode.left)
+                array.append(str(currentNode.left.val))
+            else:
+                array.append("null")
+            if currentNode.right:
+                q.append(currentNode.right)
+                array.append(str(currentNode.right.val))
+            else:
+                array.append("null")
+            size -= 1
+    while array[-1] == "null":
+        array.pop()
+    return array
+
+
+def displayErrorMessage(testCase, actualOutput, expectedOutput):
+    print("Result: Failed for test case: " + testCase)
+    print("Actual Output: ", actualOutput)
+    print("Expected Output: ", expectedOutput)
