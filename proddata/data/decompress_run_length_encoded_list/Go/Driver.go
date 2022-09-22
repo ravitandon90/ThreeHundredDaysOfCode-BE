f380c9350dc7aa01fde9703c28944fa395cbe455
Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>
2022-09-22 22:10:37 +0530

    Add driver programs in Go for problems from 17th August to 23rd August

---
+++ proddata/data/decompress_run_length_encoded_list/Go/Driver.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// 2
+// [1,2,3,4]
+// [2,4,4,4]
+// [1,1,2,3]
+// [1,3,3]
+
+func createIntArrayFromInput(input string) []int {
+	array := []int{}
+	inputAfterRemovingBraces := strings.TrimSpace(input[1 : len(input)-1])
+	if inputAfterRemovingBraces != "" {
+		arrayWithStringValues := strings.Split(inputAfterRemovingBraces, ",")
+		for _, stringValue := range arrayWithStringValues {
+			value, _ := strconv.Atoi(strings.TrimSpace(stringValue))
+			array = append(array, value)
+		}
+	}
+	return array
+}
+
+func areIntArraysEqual(arr1 []int, arr2 []int) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+	for ind, ele := range arr1 {
+		if ele != arr2[ind] {
+			return false
+		}
+	}
+	return true
+}
+
+func createStringFromArray(array []int) string {
+	s := "["
+	for ind, ele := range array {
+		s += strconv.Itoa(ele)
+		if ind != len(array)-1 {
+			s += ","
+		} else {
+			s += "]"
+		}
+	}
+	return s
+}
+
+func displayErrorMessage(testCase string, actualOutput string, expectedOutput string) {
+	fmt.Println("Result: Failed for test case: ", testCase)
+	fmt.Println("Actual Output: ", actualOutput)
+	fmt.Println("Expected Output: ", expectedOutput)
+}
+
+func main() {
+	bs, err := ioutil.ReadFile("../testcases.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	lines := strings.Split(string(bs), "\n")
+
+	isSolutionWrong := false
+	var actualOutput []int
+	var testCase string
+
+	for lineNumber, line := range lines[1:] {
+		line = strings.TrimSpace(line)
+		if lineNumber%2 == 0 {
+			testCase = line
+			input := createIntArrayFromInput(line)
+			actualOutput = decompressRLElist(input)
+		} else {
+			expectedOutput := createIntArrayFromInput(line)
+			if !areIntArraysEqual(actualOutput, expectedOutput) {
+				displayErrorMessage(testCase, createStringFromArray(actualOutput), line)
+				isSolutionWrong = true
+			}
+		}
+	}
+	if !isSolutionWrong {
+		fmt.Println("Result: Success")
+	}
+}
---
+++ proddata/data/number_of_good_pairs/Go/Driver.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// 3
+// [1,2,3,1,1,3]
+// 4
+// [1,1,1,1]
+// 6
+// [1,2,3]
+// 0
+
+func createIntArrayFromInput(input string) []int {
+	array := []int{}
+	inputAfterRemovingBraces := strings.TrimSpace(input[1 : len(input)-1])
+	if inputAfterRemovingBraces != "" {
+		arrayWithStringValues := strings.Split(inputAfterRemovingBraces, ",")
+		for _, stringValue := range arrayWithStringValues {
+			value, _ := strconv.Atoi(strings.TrimSpace(stringValue))
+			array = append(array, value)
+		}
+	}
+	return array
+}
+
+func displayErrorMessage(testCase string, actualOutput string, expectedOutput string) {
+	fmt.Println("Result: Failed for test case: ", testCase)
+	fmt.Println("Actual Output: ", actualOutput)
+	fmt.Println("Expected Output: ", expectedOutput)
+}
+
+func main() {
+	bs, err := ioutil.ReadFile("../testcases.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	lines := strings.Split(string(bs), "\n")
+
+	isSolutionWrong := false
+	var actualOutput int
+	var testCase string
+
+	for lineNumber, line := range lines[1:] {
+		line = strings.TrimSpace(line)
+		if lineNumber%2 == 0 {
+			testCase = line
+			input := createIntArrayFromInput(line)
+			actualOutput = numIdenticalPairs(input)
+		} else {
+			expectedOutput, _ := strconv.Atoi(line)
+			if actualOutput != expectedOutput {
+				displayErrorMessage(testCase, strconv.Itoa(actualOutput), line)
+				isSolutionWrong = true
+			}
+		}
+	}
+
+	if !isSolutionWrong {
+		fmt.Println("Result: Success")
+	}
+}
