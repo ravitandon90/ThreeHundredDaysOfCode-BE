86826e90250c4135dd45f92fe546bdc2fc3a4e4e
Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>
2022-09-25 00:49:44 +0530

    Add 10 more driver programs in Go

---
+++ proddata/data/maximum_subarray/Go/Driver.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// -2 1 -3 4 -1 2 1 -5 4
+// 6
+// 1
+// 1
+// 5 4 -1 7 8
+// 23
+
+func createIntArray(input string) []int {
+	array := []int{}
+	if input == "" {
+		return array
+	}
+	arrayWithStringValues := strings.Fields(input)
+	for _, stringValue := range arrayWithStringValues {
+		value, _ := strconv.Atoi(strings.TrimSpace(stringValue))
+		array = append(array, value)
+	}
+	return array
+}
+
+func areIntArraysEqual(arr1 []int, arr2 []int) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+	for ind, ele := range arr1 {
+		if ele != arr2[ind] {
+			return false
+		}
+	}
+	return true
+}
+
+func createStringFromArray(array []int) string {
+	if len(array) == 0 {
+		return "[]"
+	}
+	s := "["
+	for ind, ele := range array {
+		s += strconv.Itoa(ele)
+		if ind != len(array)-1 {
+			s += ","
+		} else {
+			s += "]"
+		}
+	}
+	return s
+}
+
+func displayErrorMessage(testCase string, actualOutput string, expectedOutput string) {
+	fmt.Println("Result: Failed for test case: ", testCase)
+	fmt.Println("Actual Output: ", actualOutput)
+	fmt.Println("Expected Output: ", expectedOutput)
+}
+
+func main() {
+	bs, err := ioutil.ReadFile("../testcases.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	lines := strings.Split(string(bs), "\n")
+
+	isSolutionWrong := false
+	var actualOutput int
+	var testCase string
+
+	for lineNumber, line := range lines {
+		line = strings.TrimSpace(line)
+		if lineNumber%2 == 0 {
+			testCase = line
+			input := createIntArray(line)
+			actualOutput = maxSubArray(input)
+		} else {
+			expectedOutput, _ := strconv.Atoi(line)
+			if actualOutput != expectedOutput {
+				displayErrorMessage(testCase, strconv.Itoa(actualOutput), line)
+				isSolutionWrong = true
+				break
+			}
+		}
+	}
+	if !isSolutionWrong {
+		fmt.Println("Result: Success")
+	}
+}
