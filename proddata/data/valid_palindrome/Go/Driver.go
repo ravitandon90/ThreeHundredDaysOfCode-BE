96390ae2b67f7a2a02d5e1bebc753e1e460ea148
Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>
2022-09-25 21:52:40 +0530

    Add Go driver programs for 10 problems between 10th Feb and 20th Feb

---
+++ proddata/data/valid_palindrome/Go/Driver.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// 3
+// "A man, a plan, a canal: Panama"
+// true
+// "race a car"
+// false
+// " "
+// true
+
+func displayErrorMessage(testCase string, actualOutput string, expectedOutput string) {
+	fmt.Println("Result: Failed for test case: ", testCase)
+	fmt.Println("Actual Output: ", actualOutput)
+	fmt.Println("Expected Output: ", expectedOutput)
+}
+
+func main() {
+	bs, err := ioutil.ReadFile("../testcases.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	lines := strings.Split(string(bs), "\n")
+
+	isSolutionWrong := false
+	var actualOutput bool
+	var testCase string
+
+	for lineNumber, line := range lines[1:] {
+		line = strings.TrimSpace(line)
+		if lineNumber%2 == 0 {
+			testCase = line
+			input := strings.TrimSpace(line[1 : len(line)-1])
+			actualOutput = isPalindrome(input)
+		} else {
+			expectedOutput := false
+			if line == "true" {
+				expectedOutput = true
+			}
+			if actualOutput != expectedOutput {
+				displayErrorMessage(testCase, strconv.FormatBool(actualOutput), line)
+				isSolutionWrong = true
+				break
+			}
+		}
+	}
+	if !isSolutionWrong {
+		fmt.Println("Result: Success")
+	}
+}
