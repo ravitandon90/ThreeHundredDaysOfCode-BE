fb6bd0e80f77d1a8154a0658b3fc7087a77bc386
Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>
2022-09-30 21:10:33 +0530

    Refactor 8 more Python driver programs

--- proddata/data/shifting_letters/Python/Driver.py
+++ proddata/data/shifting_letters/Python/Driver.py
@@ -1,6 +1,14 @@
 from Solution import Solution
+from proddata.python.helpers import createIntArrayFromString, displayErrorMessage
 import sys
 import os
+
+# 2
+# "abc", [3,5,9]
+# "rpl"
+# "aaa", [1,2,3]
+# "gfd"
+#
 
 file = open(os.path.join(sys.path[0], '../testcases.txt'), 'r')
 lines = file.readlines()
@@ -11,19 +19,17 @@ solution = Solution()
 for line in lines[1:]:
     line = line.strip()
     if lineNumber % 2 == 1:
+        testCase = line
         pos = 0
         while line[pos] != ',':
             pos += 1
         s = line[:pos][1:-1].strip()
-        shifts = list(
-            map(int, [s.strip() for s in line[pos + 1:].lstrip()[1:-1].split(',')]))
+        shifts = createIntArrayFromString(line[pos + 1:])
         actualOutput = solution.shiftingLetters(s, shifts)
     else:
         expectedOutput = line[1:-1].strip()
         if (actualOutput != expectedOutput):
-            print("Result: Failed")
-            print("Actual Output: ", actualOutput)
-            print("Expected Output: ", expectedOutput)
+            displayErrorMessage(testCase, actualOutput, expectedOutput)
             isSolutionWrong = True
             break
     lineNumber += 1
