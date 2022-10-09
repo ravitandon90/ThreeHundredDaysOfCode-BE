2cb199b2c4219da3a7d84bf12e6d5223196a49d4
Swapnil Sengupta <53232360+Swapnil-2001@users.noreply.github.com>
2022-10-10 02:56:22 +0530

    Add 10 more driver programs in Golang (#144)
    Co-authored-by: Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>

---
+++ proddata/data/find_minimum_in_rotated_sorted_array/Go/Driver.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// 3
+// [3,4,5,1,2]
+// 1
+// [4,5,6,7,0,1,2]
+// 0
+// [11,13,15,17]
+// 11
+
+func createIntArrayFromString(input string) []int {
+	array := []int{}
+	inputAfterRemovingBraces := strings.TrimSpace(input[1 : len(input)-1])
+	if inputAfterRemovingBraces != "" {
+		arrayWithStringValues := strings.Split(inputAfterRemovingBraces, ",")
+		for _, stringValue := range arrayWithStringValues {
+			value, _ := strconv.Atoi(strings.TrimSpace(stringValue))
+			array = append(array, value)
+		}
+	}
+	return array
+}
+
+func displayErrorMessage(testCase string, actualOutput string, expectedOutput string) {
+	fmt.Println("Result: Failed for test case: ", testCase)
+	fmt.Println("Actual Output: ", actualOutput)
+	fmt.Println("Expected Output: ", expectedOutput)
+}
+
+func main() {
+	bs, err := ioutil.ReadFile("../testcases.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	lines := strings.Split(string(bs), "\n")
+
+	isSolutionWrong := false
+	var actualOutput int
+	var testCase string
+
+	for lineNumber, line := range lines[1:] {
+		line = strings.TrimSpace(line)
+		if lineNumber%2 == 0 {
+			testCase = line
+			nums := createIntArrayFromString(line)
+			actualOutput = findMin(nums)
+		} else {
+			expectedOutput, _ := strconv.Atoi(line)
+			if actualOutput != expectedOutput {
+				displayErrorMessage(testCase, strconv.Itoa(actualOutput), line)
+				isSolutionWrong = true
+				break
+			}
+		}
+	}
+	if !isSolutionWrong {
+		fmt.Println("Result: Success")
+	}
+}
---
+++ proddata/data/longest_consecutive_sequence/Go/Driver.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// 2
+// [100,4,200,1,3,2]
+// 4
+// [0,3,7,2,5,8,4,6,0,1]
+// 9
+
+func createIntArrayFromString(input string) []int {
+	array := []int{}
+	inputAfterRemovingBraces := strings.TrimSpace(input[1 : len(input)-1])
+	if inputAfterRemovingBraces != "" {
+		arrayWithStringValues := strings.Split(inputAfterRemovingBraces, ",")
+		for _, stringValue := range arrayWithStringValues {
+			value, _ := strconv.Atoi(strings.TrimSpace(stringValue))
+			array = append(array, value)
+		}
+	}
+	return array
+}
+
+func displayErrorMessage(testCase string, actualOutput string, expectedOutput string) {
+	fmt.Println("Result: Failed for test case: ", testCase)
+	fmt.Println("Actual Output: ", actualOutput)
+	fmt.Println("Expected Output: ", expectedOutput)
+}
+
+func main() {
+	bs, err := ioutil.ReadFile("../testcases.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	lines := strings.Split(string(bs), "\n")
+
+	isSolutionWrong := false
+	var actualOutput int
+	var testCase string
+
+	for lineNumber, line := range lines[1:] {
+		line = strings.TrimSpace(line)
+		if lineNumber%2 == 0 {
+			testCase = line
+			nums := createIntArrayFromString(line)
+			actualOutput = longestConsecutive(nums)
+		} else {
+			expectedOutput, _ := strconv.Atoi(line)
+			if actualOutput != expectedOutput {
+				displayErrorMessage(testCase, strconv.Itoa(actualOutput), line)
+				isSolutionWrong = true
+				break
+			}
+		}
+	}
+	if !isSolutionWrong {
+		fmt.Println("Result: Success")
+	}
+}
