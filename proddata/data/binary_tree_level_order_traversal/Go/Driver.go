92eccaf30ccd207656abb799789b63f1a63e90a8
Swapnil Sengupta <53232360+Swapnil-2001@users.noreply.github.com>
2022-10-10 02:56:27 +0530

    7 more driver programs added in Golang (#143)
    * Add Go driver programs for tree problems
    
    * Add 7 more driver programs in Golang
    
    Co-authored-by: Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>

---
+++ proddata/data/binary_tree_level_order_traversal/Go/Driver.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// 3
+// [3,9,20,null,null,15,7]
+// [[3],[9,20],[15,7]]
+// [1]
+// [[1]]
+// []
+// []
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func createTreeFromArray(input string) *TreeNode {
+	inputWithNoParentheses := strings.TrimSpace(input[1 : len(input)-1])
+	if inputWithNoParentheses == "" {
+		return nil
+	}
+	stringNodes := strings.Split(inputWithNoParentheses, ",")
+	for ind, stringNode := range stringNodes {
+		stringNodes[ind] = strings.TrimSpace(stringNode)
+	}
+	return createTree(stringNodes)
+}
+
+func createTree(stringNodes []string) *TreeNode {
+	rootVal, _ := strconv.Atoi(stringNodes[0])
+	root := &TreeNode{rootVal, nil, nil}
+	posInArray := 1
+	var queue []*TreeNode
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		size := len(queue)
+		for size > 0 {
+			if posInArray == len(stringNodes) {
+				queue = make([]*TreeNode, 0)
+				break
+			}
+			currentNode := queue[0]
+			var nodeVal int
+			if stringNodes[posInArray] != "null" {
+				nodeVal, _ = strconv.Atoi(stringNodes[posInArray])
+				currentNode.Left = &TreeNode{nodeVal, nil, nil}
+				queue = append(queue, currentNode.Left)
+			}
+			posInArray++
+			if posInArray == len(stringNodes) {
+				queue = make([]*TreeNode, 0)
+				break
+			}
+			if stringNodes[posInArray] != "null" {
+				nodeVal, _ = strconv.Atoi(stringNodes[posInArray])
+				currentNode.Right = &TreeNode{nodeVal, nil, nil}
+				queue = append(queue, currentNode.Right)
+			}
+			posInArray++
+			size--
+			queue[0] = nil
+			queue = queue[1:]
+		}
+	}
+	return root
+}
+
+func createIntMatrixFromString(s string) [][]int {
+	stringWithoutOuterBraces := strings.TrimSpace(s[1 : len(s)-1])
+	mat := [][]int{}
+	if stringWithoutOuterBraces == "" {
+		return mat
+	}
+	arrays := strings.Split(stringWithoutOuterBraces[:len(stringWithoutOuterBraces)-1], "]")
+	for _, arr := range arrays {
+		for i, ch := range arr {
+			if byte(ch) == '[' {
+				row := []int{}
+				if strings.TrimSpace(arr[i+1:]) != "" {
+					stringNums := strings.Split(arr[i+1:], ",")
+					for _, stringNum := range stringNums {
+						num, _ := strconv.Atoi(strings.TrimSpace(stringNum))
+						row = append(row, num)
+					}
+				}
+				mat = append(mat, row)
+				break
+			}
+		}
+	}
+	return mat
+}
+
+func areIntArraysEqual(arr1 []int, arr2 []int) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+	for ind, ele := range arr1 {
+		if ele != arr2[ind] {
+			return false
+		}
+	}
+	return true
+}
+
+func areIntMatricesEqual(mat1 [][]int, mat2 [][]int) bool {
+	if len(mat1) != len(mat2) {
+		return false
+	}
+	for pos, array := range mat1 {
+		if !areIntArraysEqual(array, mat2[pos]) {
+			return false
+		}
+	}
+	return true
+}
+
+func createStringFromArray(array []int) string {
+	if len(array) == 0 {
+		return "[]"
+	}
+	s := "["
+	for ind, ele := range array {
+		s += strconv.Itoa(ele)
+		if ind != len(array)-1 {
+			s += ","
+		} else {
+			s += "]"
+		}
+	}
+	return s
+}
+
+func createStringFromMatrix(matrix [][]int) string {
+	if len(matrix) == 0 {
+		return "[]"
+	}
+	s := "["
+	for ind, array := range matrix {
+		s += createStringFromArray(array)
+		if ind != len(matrix)-1 {
+			s += ","
+		} else {
+			s += "]"
+		}
+	}
+	return s
+}
+
+func displayErrorMessage(testCase string, actualOutput string, expectedOutput string) {
+	fmt.Println("Result: Failed for test case: ", testCase)
+	fmt.Println("Actual Output: ", actualOutput)
+	fmt.Println("Expected Output: ", expectedOutput)
+}
+
+func main() {
+	bs, err := ioutil.ReadFile("../testcases.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	lines := strings.Split(string(bs), "\n")
+
+	isSolutionWrong := false
+	var actualOutput [][]int
+	var testCase string
+
+	for lineNumber, line := range lines[1:] {
+		line = strings.TrimSpace(line)
+		if lineNumber%2 == 0 {
+			testCase = line
+			tree := createTreeFromArray(line)
+			actualOutput = levelOrder(tree)
+		} else {
+			expectedOutput := createIntMatrixFromString(line)
+			if !areIntMatricesEqual(actualOutput, expectedOutput) {
+				isSolutionWrong = true
+				displayErrorMessage(testCase, createStringFromMatrix(actualOutput), line)
+				break
+			}
+		}
+	}
+	if !isSolutionWrong {
+		fmt.Println("Result: Success")
+	}
+}
