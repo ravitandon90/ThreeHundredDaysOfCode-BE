49483056d247f0ee95611776129ff91ec993d099
Swapnil Sengupta <53232360+Swapnil-2001@users.noreply.github.com>
2022-10-10 02:56:11 +0530

    Add driver programs for 5 more problems in Golang (#146)
    Co-authored-by: Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>

---
+++ proddata/data/check_if_it_is_a_straight_line/Go/Driver.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// 2
+// [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+// true
+// [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+// false
+
+func createIntMatrixFromString(s string) [][]int {
+	stringWithoutOuterBraces := strings.TrimSpace(s[1 : len(s)-1])
+	mat := [][]int{}
+	if stringWithoutOuterBraces == "" {
+		return mat
+	}
+	arrays := strings.Split(stringWithoutOuterBraces[:len(stringWithoutOuterBraces)-1], "]")
+	for _, arr := range arrays {
+		for i, ch := range arr {
+			if byte(ch) == '[' {
+				row := []int{}
+				if strings.TrimSpace(arr[i+1:]) != "" {
+					stringNums := strings.Split(arr[i+1:], ",")
+					for _, stringNum := range stringNums {
+						num, _ := strconv.Atoi(strings.TrimSpace(stringNum))
+						row = append(row, num)
+					}
+				}
+				mat = append(mat, row)
+				break
+			}
+		}
+	}
+	return mat
+}
+
+func displayErrorMessage(testCase string, actualOutput string, expectedOutput string) {
+	fmt.Println("Result: Failed for test case: ", testCase)
+	fmt.Println("Actual Output: ", actualOutput)
+	fmt.Println("Expected Output: ", expectedOutput)
+}
+
+func main() {
+	bs, err := ioutil.ReadFile("../testcases.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	lines := strings.Split(string(bs), "\n")
+
+	isSolutionWrong := false
+	var actualOutput bool
+	var testCase string
+
+	for lineNumber, line := range lines[1:] {
+		line = strings.TrimSpace(line)
+		if lineNumber%2 == 0 {
+			testCase = line
+			coordinates := createIntMatrixFromString(line)
+			actualOutput = checkStraightLine(coordinates)
+		} else {
+			expectedOutput := true
+			if line == "false" {
+				expectedOutput = false
+			}
+			if actualOutput != expectedOutput {
+				displayErrorMessage(testCase, strconv.FormatBool(actualOutput), line)
+				isSolutionWrong = true
+				break
+			}
+		}
+	}
+	if !isSolutionWrong {
+		fmt.Println("Result: Success")
+	}
+}
