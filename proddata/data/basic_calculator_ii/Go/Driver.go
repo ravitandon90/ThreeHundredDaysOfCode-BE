aba4a6ec67bb9039cbe6117b8ec2157142595e75
Swapnil Sengupta <53232360+Swapnil-2001@users.noreply.github.com>
2022-10-28 21:00:53 +0530

    Add driver programs for 8 more problems in Golang (#155)
    Co-authored-by: Swapnil Sengupta <swapnilsengupta@Swapnils-MacBook-Air.local>

---
+++ proddata/data/basic_calculator_ii/Go/Driver.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// 3+2*2
+// 7
+//  3/2
+// 1
+//  3+5 / 2
+// 5
+
+func displayErrorMessage(testCase string, actualOutput string, expectedOutput string) {
+	fmt.Println("Result: Failed for test case: ", testCase)
+	fmt.Println("Actual Output: ", actualOutput)
+	fmt.Println("Expected Output: ", expectedOutput)
+}
+
+func main() {
+	bs, err := ioutil.ReadFile("../testcases.txt")
+	if err != nil {
+		os.Exit(1)
+	}
+	lines := strings.Split(string(bs), "\n")
+
+	isSolutionWrong := false
+	var actualOutput int
+	var testCase string
+
+	for lineNumber, line := range lines {
+		line = strings.TrimSpace(line)
+		if lineNumber%2 == 0 {
+			testCase = line
+			actualOutput = calculate(line)
+		} else {
+			expectedOutput, _ := strconv.Atoi(line)
+			if actualOutput != expectedOutput {
+				displayErrorMessage(testCase, strconv.Itoa(actualOutput), line)
+				isSolutionWrong = true
+				break
+			}
+		}
+	}
+	if !isSolutionWrong {
+		fmt.Println("Result: Success")
+	}
+}
